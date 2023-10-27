504946c6968133763bbb1b392e6095e1a8c239fe
vnncoo <145742841+vnncoo@users.noreply.github.com>
2023-10-27 19:40:59 +0700

    Add files via upload

---
+++ Introduction to CompSci/Week-1/mario-more/mario.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int h;
+    do
+    {
+        h = get_int("Height: ");
+    }
+    while (h < 1 || h > 8);
+
+    for (int r = 0; r < h; r++)
+    {
+        for (int d = 0; d < h - r - 1; d++)
+        {
+            printf(" ");
+        }
+        for (int c = 0; c <= r; c++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (int c = 0; c <= r; c++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
