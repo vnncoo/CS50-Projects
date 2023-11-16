8904a52256df35e5e7bebe12f094fbdc6cd583f4
Ivann Comiso <vnncoo@gmail.com>
2023-11-17 03:22:31 +0700

    upd.itc-hello-imp
    Line 9-14 is unnecessary, gonna have to continue working on it.

--- Introduction to CompSci/Week-1/hello.c
+++ Introduction to CompSci/Week-1/hello.c
@@ -4,6 +4,12 @@
 int main(void)
 {
     string name = get_string("What's your name? ");
-
     printf("hello, %s\n", name);
+
+    int length = strlen(name);
+    
+    for (int i = 0; i < length; i++)
+        {
+            
+        }
 }
